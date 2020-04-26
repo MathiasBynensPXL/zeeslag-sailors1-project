d6559db64d9801d2f4b033efc6976548bc80b9a6
AlejandroCouwetPXL <alejandro.couwet>
2020-04-26 14:30:42 +0200

    acceptatie criteria bijwerkingen.

--- Project/Source/Battleship.Api/wwwroot/Function.js
+++
@@ -1,6 +0,0 @@
-﻿function CheckToken() {
-    if (sessionStorage.getItem("token") != null) {
-        return;
-    }
-    window.location.href = "https://localhost:5001/login_user.html";
-}
---
+++ Project/Source/Battleship.Api/wwwroot/assets/Function.js
@@ -0,0 +1,41 @@
+﻿function logFunction() {
+
+    let email = document.getElementById('email').value;
+    let paswoord = document.getElementById('paswoord').value;
+    let user = { email: email, password: paswoord };
+    let url = 'https://localhost:5001/api/Authentication/token';
+    let is_loggedin = false;
+    fetch(url,
+        {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+            }
+        })
+        .then((response) => {
+            if (response.status == 200) {
+                response.json().then(data => {
+                    sessionStorage.setItem("token", data.token);
+                    window.location.href = "https://localhost:5001/index.html";
+                });
+
+            } else {
+                alert("foute inloggegevens, probeer opnieuw!")
+                throw `error with status ${response.status}`;
+            }
+        })
+        .then((user) => {
+            let data = [];
+            data.push([user.email, user.password]);
+        });
+}
+
+
+function CheckToken() {
+    if (sessionStorage.getItem("token") == null) {
+        window.location.href = "https://localhost:5001/login_user.html";
+    }
+}
