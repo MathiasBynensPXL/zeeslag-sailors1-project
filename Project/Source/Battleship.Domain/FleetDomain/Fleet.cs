848cde3ade4c15cd4df8f460a2d179a75484043c
AlejandroCouwetPXL <alejandro.couwet>
2020-05-03 16:49:00 +0200

    To-do list fleet completed

--- Project/Source/Battleship.Domain/FleetDomain/Fleet.cs
+++ Project/Source/Battleship.Domain/FleetDomain/Fleet.cs
@@ -24,7 +24,45 @@ namespace Battleship.Domain.FleetDomain
 
         public Result TryMoveShipTo(ShipKind kind, GridCoordinate[] segmentCoordinates, IGrid grid)
         {
-            throw new NotImplementedException("TryMoveShipTo method of Fleet class is not implemented");
+            if (!GridCoordinateArrayExtensions.AreAligned(segmentCoordinates))
+            {
+                return Result.CreateFailureResult("Coordinates are not aligned!");
+            }
+            if (!GridCoordinateArrayExtensions.AreLinked(segmentCoordinates))
+            {
+                return Result.CreateFailureResult("Coordinates are not linked!");
+            }
+            if (segmentCoordinates.Length != kind.Size)
+            {
+                return Result.CreateFailureResult("SegmentCoordinates do not match length of ShipKind!");
+            } 
+            if (segmentCoordinates.HasAnyOutOfBounds(grid.Size))
+            {
+                return Result.CreateFailureResult("1 or More Coordinate(s) are outside of the grid!");
+            } 
+            foreach (IShip ship in _Dictionary.Values)
+            {
+                for (int i = 0; i < segmentCoordinates.Length; i++)
+                {
+                    if (ship.CanBeFoundAtCoordinate(segmentCoordinates[i]))
+                    {
+                        return Result.CreateFailureResult("1 or more Coordinate(s) collide with another ship");
+                    }
+                }
+            }
+            foreach(IShip ship in _Dictionary.Values) {
+                if (ship.Kind == kind)
+                {
+                    IGridSquare[] converted = ConvertGridCoordinateToGridSquare(segmentCoordinates);
+                    ship.PositionOnGrid(converted);
+                }
+            }
+            for (int i = 0; i < segmentCoordinates.Length; i++)
+            {
+
+                grid.GetSquareAt(segmentCoordinates[i]);
+            }
+            return Result.CreateSuccessResult();
         }
 
         public void RandomlyPositionOnGrid(IGrid grid, bool allowDeformedShips = false)
@@ -48,5 +86,16 @@ namespace Battleship.Domain.FleetDomain
         {
             throw new NotImplementedException("GetSunkenShips method of Fleet class is not implemented");
         }
+
+        private IGridSquare[] ConvertGridCoordinateToGridSquare(GridCoordinate[] gridCoordinates)
+        {
+            GridSquare[] gridSquare = new GridSquare[gridCoordinates.Length];
+            for (int i = 0; i < gridCoordinates.Length; i++)
+            {
+                gridSquare[i] = new GridSquare(gridCoordinates[i]);
+            }
+            return gridSquare;
+        }
+
     }
 }
