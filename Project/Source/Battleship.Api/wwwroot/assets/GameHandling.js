8131309a2c65165f8b4e7425b6d5a5995cd28afa
Anton Steegmans <11902529@student.pxl.be>
2020-05-10 17:46:13 +0200

    Stats toegevoegd

--- Project/Source/Battleship.Api/wwwroot/assets/GameHandling.js
+++ Project/Source/Battleship.Api/wwwroot/assets/GameHandling.js
@@ -30,9 +30,23 @@ function IsStarted() {
 
     errorMessage();
     if (sessionStorage.getItem("isStarted") === 'true') {
+            
             sessionStorage.setItem("numberOfShots", 0);
+            sessionStorage.setItem("shots_hit", 0);
+            sessionStorage.setItem("shots_miss", 0);
+
+
             let knopReady = document.getElementById('Start');
-             knopReady.style.visibility = 'hidden';
+            knopReady.style.visibility = 'hidden';
+
+            let knopReset = document.getElementById('btnReset');
+            knopReset.style.visibility = 'hidden';
+
+            let boatlist = document.getElementById('BoatList');
+            boatlist.style.visibility = 'hidden';
+
+            let stats = document.getElementById('Stats');
+            stats.style.visibility = 'visible';
 
             const buttons = document.querySelectorAll('.btnComputer');
             buttons.forEach(function (currentBtn)
@@ -40,7 +54,6 @@ function IsStarted() {
                 currentBtn.addEventListener('click', findCoordinateAndShoot);
             });
     }
-
 }
 
 function findCoordinateAndShoot() {
@@ -90,13 +103,34 @@ function stats() {
     let numberOfShots = sessionStorage.getItem("numberOfShots");
     numberOfShots++;
     sessionStorage.setItem("numberOfShots", numberOfShots);
+    let shots_fired = document.getElementById("stat_fired");
+    shots_fired.value = numberOfShots;
+
+    let shot_hit = sessionStorage.getItem('shots_hit');
+    let shit_miss = sessionStorage.getItem('shots_miss');
+
+    if (sessionStorage.getItem("shotResult") === 'true') {
+        shot_hit++;
+        sessionStorage.setItem("shots_hit", shot_hit);
+    } else {
+        shit_miss++;
+        sessionStorage.setItem("shots_miss", shit_miss);
+    }
+    let hit = document.getElementById("stat_hit");
+    let miss = document.getElementById("stat_missed");
+    let ratio = document.getElementById("stat_ratio");
+
+    hit.value = shot_hit;
+    miss.value = shit_miss;
+    let sommetje = shot_hit / numberOfShots;
+    ratio.value = sommetje.toFixed(2);
 }
 
 function drawShots() {
     let coordinaat = sessionStorage.getItem("shotPlayer");
     let isAHit = sessionStorage.getItem("shotResult") === 'true';
     let id = document.getElementById(coordinaat);
-    if (sessionStorage.getItem("shotResult") === 'true') {
+    if (isAHit) {
         id.className = "Hit";
     } else {
         id.className = "Miss";
